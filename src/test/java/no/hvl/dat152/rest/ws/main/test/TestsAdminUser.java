6ffb5dfa6151cf42d25c1fa5b87fb3bef73bb17a
Simon Skarboe <599000@stud.hvl.no>
2023-10-22 11:59:20 +0200

    TestAdmin fixed

--- src/test/java/no/hvl/dat152/rest/ws/main/test/TestsAdminUser.java
+++ src/test/java/no/hvl/dat152/rest/ws/main/test/TestsAdminUser.java
@@ -18,12 +18,7 @@ class TestsAdminUser {
 	
 	private String API_ROOT = "http://localhost:8090/elibrary/api/v1/admin";
 	
-	private String token = "eyJhbGciOiJSUzI1NiJ9.eyJzdWIiOiJiZXJpdEBlbWFpbC5jb20iLCJpc3MiOiJEQVQxNTItTGVjdHVyZXJAVERPWSIs"
-			+ "ImZpcnN0bmFtZSI6IkJlcml0IiwibGFzdG5hbWUiOiJKw7hyZ2Vuc2VuIiwicm9sZXMiOlsiU1VQRVJfQURNSU4iLCJVU0VSIiwiQURNSU"
-			+ "4iXSwiaWF0IjoxNjk2MjAwMTk3LCJleHAiOjE2OTYyODY1OTd9.eLWV51s4rtauq04wiB_ohBMdpfC7k3D9gHPpIqPUmFinV_VBvI0iuHT"
-			+ "ev4Xc7Sq5qiSV7aAG3abY96Hu-mG5r23EVFy1Ipwq8t_02-mCzvIcSh25TlcHWUtfzKqM2UPcAwnvZnpvjhDYyPUnKgTN42uZn-msSO0N3"
-			+ "zo8XT859Eylh2OsRWKla8UYXEz_GttR4cQ1--0-aIIAvOgYk8HmgriOIpQs4WuolzfLsMelk0xEo_C5vMv3XEpaWrKOOhjUl7GwqqkR7wT"
-			+ "_yISPlLHj28ZcUAgGbKa_R73Fm8br8tgwOaz_nHpi25Nf_VAlqeBKsb5Z8rykM1WVzb9Va242IA";
+	private String token = "eyJhbGciOiJSUzI1NiJ9.eyJzdWIiOiJiZXJpdEBlbWFpbC5jb20iLCJpc3MiOiJEQVQxNTItTGVjdHVyZXJAVERPWSIsImZpcnN0bmFtZSI6IkJlcml0IiwibGFzdG5hbWUiOiJKw7hyZ2Vuc2VuIiwicm9sZXMiOlsiVVNFUiIsIlNVUEVSX0FETUlOIiwiQURNSU4iXSwiaWF0IjoxNjk3NzQxNTEyLCJleHAiOjE2OTgxNzM1MTJ9.gBWbP_2_KQPyNuyjFthMgNub9iu72rtm6kWN3jRZbDl6WiIn3YJgl6COicKl_N8TMzQx_J2v62RxMQ_GY8ZQlWXx41Rl9l1hd1iC40VrIxdQaBFyEsmA_05qn8AXkNh001Em4c1m3F1X7Eky5149GohaSPhz_RlxFtD6X9uYaabetRwmhPJYz0qEyjjfD5yGg_RdQPOmvcX7DMEPfGxnDQMYdWbahUo6zhcVPF6hH-ovUO2EIbJWBVY17_KlbcaK8rS1kJwUlC-76rRTb6i89lazeCFhiQt-X1tNeY_P7Avrjz0FekLZQBfBh3bxuD4Cek1aUohrMtShB8W-QaxBfQ";
 	
 	@DisplayName("JUnit test for @PutMapping(/users/{id}) endpoint")
 	@Test
@@ -32,11 +27,11 @@ class TestsAdminUser {
 		Response response = RestAssured.given()
 				.header("Authorization", "Bearer "+ token)
 				.contentType(MediaType.APPLICATION_JSON_VALUE)
-				.queryParam("role", "admin")
+				.queryParam("role", "ADMIN")
 				.put(API_ROOT+"/users/{id}", 1);
 	    
 	    assertEquals(HttpStatus.OK.value(), response.getStatusCode());
-	    assertEquals("@email.com", response.jsonPath().get("email"));
+	    assertEquals("robert@email.com", response.jsonPath().get("email"));
 	}
 	
 	@DisplayName("JUnit test for @DeleteMapping(/users/{id}) endpoint")
@@ -45,7 +40,7 @@ class TestsAdminUser {
 		
 		Response response = RestAssured.given()
 				.header("Authorization", "Bearer "+ token)
-				.queryParam("role", "user")
+				.queryParam("role", "USER")
 				.delete(API_ROOT+"/users/{id}", 1);
 	    
 	    assertEquals(HttpStatus.OK.value(), response.getStatusCode());
